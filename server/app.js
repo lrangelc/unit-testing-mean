18f09565c0ff5f95af96e7363326536cfd6f32a9
Luis Rangel Castro <luisrangelc@rpaipos.com>
2020-09-08 09:49:22 -0600

    add test get

--- server/app.js
+++ server/app.js
@@ -1,37 +1,40 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
 
-var apiRouter = require('./routes/pins').router;
+var apiRouter = require("./routes/pins").router;
 
 var app = express();
 
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 mongoose
-  .set('useFindAndModify', false)
-  .connect('mongodb://localhost/pins', { promiseLibrary: require('bluebird'), useNewUrlParser: true })
-  .then(() => console.log('connection successful'))
-  .catch(err => console.error(err));
+  .set("useFindAndModify", false)
+  .connect("mongodb://localhost/pins", {
+    promiseLibrary: require("bluebird"),
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("connection successful"))
+  .catch((err) => console.error(err));
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'dist/pin')));
-app.use('/', express.static(path.join(__dirname, 'dist/pin')));
-app.use('/api', apiRouter);
+app.use(express.static(path.join(__dirname, "dist/pin")));
+app.use("/", express.static(path.join(__dirname, "dist/pin")));
+app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
