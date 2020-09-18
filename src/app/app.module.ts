da82e792f064cb64ada60fcb16cb5d03362ec96f
Luis Rangel Castro <luisrangelc@rpaipos.com>
2020-09-18 14:05:41 -0600

    upgrade 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
-import {
-  MatToolbarModule,
-  MatButtonModule,
-  MatSidenavModule,
-  MatIconModule,
-  MatListModule,
-  MatInputModule,
-  MatSelectModule,
-  MatRadioModule,
-  MatCardModule,
-  MatNativeDateModule,
-  MatExpansionModule,
-  MatCheckboxModule,
-  MatBottomSheetModule,
-  MatStepperModule,
-  MatSnackBarModule
-} from '@angular/material';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { PinsComponent } from './components/pins/pins.component';
 import { LayoutComponent } from './components/layout/layout.component';
